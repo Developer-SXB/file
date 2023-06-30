bd635743f83e36c9ee875c3d53240d8a30d0063b
Developer-SXB <138223426+Developer-SXB@users.noreply.github.com>
2023-06-30 18:15:06 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,7 +1,9 @@
 import os
 try:import requests
-except:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-#os.system('xdg-open https://chat.whatsapp.com/FwFmZyWx4X3GQyxV4jEtSU')
+except:
+  os.system('clear')
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+os.system('xdg-open https://www.facebook.com/profile.php?id=100090830941440')
 os.system('git pull')
 from platform import uname
 bt=uname().machine.lower()
